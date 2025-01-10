a54fd4be9a52c042904c220fe9561b473bb9761c
aoshiguchen <1052045476@qq.com>
2025-01-10 14:16:27 +0800

    init.

--- scripts/build/JCudaxNativesBuild-12.0.0.sh
+++ scripts/build/JCudaxNativesBuild-12.0.0.sh
@@ -3,11 +3,9 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Hom
 export MAVEN_HOME=/Users/yangwen/my/service/maven/apache-maven-3.8.1
 export PATH=:$PATH:$JAVA_HOME/bin:$MAVEN_HOME/bin
 
-
 version=12.0.0
 rootDir=$PWD/../..
-workDir=$rootDir/jcudax-$version/src/main/java
-headerFileDir=$rootDir/jcudax-natives-$version/src/main/c
+workDir=$rootDir
 
 cd $workDir
-javah -d $headerFileDir org.dromara.jcudax.JCudax
+mvn clean package -pl jcudax-natives-${version} -P windows-x86_64
